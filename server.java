853acdd8aede7a088fc5df6882f7a3b5bc217246
liasyamsurial <48401025+liasyamsurial@users.noreply.github.com>
2019-03-20 16:32:15 +0800

    Create server.java

---
+++ server.java
@@ -0,0 +1,42 @@
+import java.net.*;
+import java.io.*;
+
+public class GreetingServer {
+
+	public static void main(String[] args) throws IOException {
+
+	     ServerSocket sockets = new ServerSocket(6800);
+	     Socket serSoc = sockets.accept();
+	
+             System.out.println("Waiting for client on port: " + serSoc.getLocalPort());
+	     
+             System.out.println("Just connected to: " + serSoc.getRemoteSocketAddress());
+
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+       
+             OutputStream ostream = serSoc.getOutPutStream();
+
+             PrintWriter writer = new PrintWriter(ostream, true);
+	     writer.println("Hello Client!");
+
+             InputStream input = serSoc.getInputStream();
+
+             BufferedReader recvRead = new BufferedReader(new InputStreamReader(input));
+             
+             String receiveMessage, sendMessage;
+             
+             While(true)
+             {
+                if((receiveMessage = recvRead.readLine()) != null)
+                {
+                  System.out.println("Server connected!");
+                  System.out.println(receiveMessage);
+                }
+  
+             sendMessage = reader.readLine();
+             writer.println(sendMessage);
+             writer.flush();
+}
+}
+}
