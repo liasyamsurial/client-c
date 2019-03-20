4afe75a1982c58a5474e8ebbbc7ecec4d6389d61
liasyamsurial <48401025+liasyamsurial@users.noreply.github.com>
2019-03-20 16:31:29 +0800

    Create client.c

---
+++ client.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+int main()
+{
+    
+
+	int sockid = socket(AF_INET, SOCK_STREAM, 0);
+    char server_msg[300] = "Hi Server!";
+
+    struct sockaddr_in addrport ;    
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(6800);
+    addrport.sin_addr.s_addr = inet_addr("10.0.2.10");
+
+   
+    int status = connect(sockid, (struct sockaddr *) &addrport, sizeof(addrport));
+
+    if (status == -1) {
+
+       printf("There was an error %s\n");
+    }
+
+    send(sockid, server_msg, sizeof(server_msg),0);
+    char server_response[300];
+    recv(sockid, &server_response, sizeof(server_response), 0);
+  
+    printf("server message: %s\n", server_response);
+
+
+
+
+	close(sockid);
+
+	return 0;
+
+}
